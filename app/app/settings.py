f86590937c36937f264a422700c97a090cb59b0b
hainguyen-namiq <hainguyen@i3-automation.com>
2024-03-17 15:59:59 +0700

    Pull settings from env vars.

--- app/app/settings.py
+++ app/app/settings.py
@@ -19,13 +19,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0xs+7-i#ad=ss*%h)2a3qp2+x5o^*ik%jm(z-c)&jpgm8i2(v+'
+# SECRET_KEY = 'django-insecure-0xs+7-i#ad=ss*%h)2a3qp2+x5o^*ik%jm(z-c)&jpgm8i2(v+'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 # Application definition
 
 INSTALLED_APPS = [
